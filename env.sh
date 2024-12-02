764e5421924535414e2a674f8e459b4b165b6a28
Sungkeun Kim <sk84.kim@samsung.com>
2024-12-02 10:16:32 +0900

    Remove unnecessary file and code

--- env.sh
+++ env.sh
@@ -5,12 +5,6 @@ cd chipyard-1.11.0
 source env.sh
 cd ..
 
-# Unpack prebuild images
-cd prebuilt
-./unpack.sh
-cd ..
-
-
 # Set cross compiler for building fw_payload
 path_to_gcc=`which riscv64-unknown-elf-gcc`
 export CROSS_COMPILE=$(echo "$path_to_gcc" | sed 's/\(.*\)gcc/\1/')
